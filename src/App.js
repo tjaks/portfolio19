6e5864bcb443b6acda83a67916563328a6d82347
tommyjaks <tommy.jaks@quedro.com>
2019-03-28 09:08:58 +0100

    predloader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Preloader, Placeholder } from 'react-preloading-screen';
 import './App.scss';
 import Home from './components/home';
 import Footer from './components/footer';
@@ -16,23 +17,32 @@ import nookstockholmWebsite from './components/case/nookstockholm-website';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <BrowserRouter>
+            <Preloader>
+                <div className="App">
+                    <BrowserRouter>
+                        <div className="animations">
+                            <Header/>
+                            <Switch>
+                                <Route path="/case/nookstockholm" component={ nookstockholmWebsite }/>
+                                <Route path="/case/smartbokrwebsite" component={ smartbokrWebsite }/>
+                                <Route path="/case/smartbokrapp" component={ smartbokrApp }/>
+                                <Route path="/case/refitness" component={ refitness }/>
+                                <Route path="/case/smartbokr-webb" component={ smartbokrWebb }/>
+                                <Route path="/about" component={ About }/>
+                                <Route path="/" component={ Home }/>
+                            </Switch>
+                            <Footer/>
+                        </div>
+                    </BrowserRouter>
+                </div>
+                <Placeholder>
                     <div className="animations">
-                        <Header/>
-                        <Switch>
-                            <Route path="/case/nookstockholm" component={ nookstockholmWebsite }/>
-                            <Route path="/case/smartbokrwebsite" component={ smartbokrWebsite }/>
-                            <Route path="/case/smartbokrapp" component={ smartbokrApp }/>
-                            <Route path="/case/refitness" component={ refitness }/>
-                            <Route path="/case/smartbokr-webb" component={ smartbokrWebb }/>
-                            <Route path="/about" component={ About }/>
-                            <Route path="/" component={ Home }/>
-                        </Switch>
-                        <Footer/>
+                        <div className="startpage__headline">
+                            <h1 style={{fontSize: '1.7rem', margin: '.5rem 0 0 0'}}>Digital Product Designer <br/> based in Stockholm.</h1>
+                        </div>
                     </div>
-                </BrowserRouter>
-            </div>
+                </Placeholder>
+            </Preloader>
         );
     }
 }
